c32afa4a39bec0b31938061c951b415943e0a3b5
Mac Imran <lartane@gmail.com>
2016-05-02 20:14:55 +0200

    j = 1

--- src/main/java/org/imran/simpleApp/App.java
+++ src/main/java/org/imran/simpleApp/App.java
@@ -19,7 +19,7 @@ import fr.ign.cogit.geoxygene.util.conversion.WktGeOxygene;
 public class App {
   public static void doSomething() {
     System.out.println("el loko molo polo");
-    int j = 2;
+    int j = 1;
   }
 
   public static void main(String[] args) throws ParseException {
